bc883f5c140e41cee6aa4ce29186a653aad3e2c2
Esther12 <yaringsu@foxmail.com>
2017-06-14 12:02:35 -0400

    Add files via upload

---
+++ AccProject/src/entry/Entry.java
@@ -0,0 +1,17 @@
+package entry;
+
+import javax.swing.SwingUtilities;
+
+import view.MainFrame;
+
+
+public class Entry {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				new MainFrame();
+			}
+		});
+	}
+}
